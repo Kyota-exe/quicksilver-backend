4ccfac3c931d7ce780e5ef85ae8c0ebf79fa0434
Kyota Poeti <kyota.poeti@icloud.com>
2025-11-15 23:25:02 -0500

    Register Project in admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "owner", "status", "created_at")
+    list_filter = ("status", "owner")
+    search_fields = ("name", "description")
