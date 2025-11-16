485af8556439d075ce06ed364609bc31e56ce502
Kyota Poeti <kyota.poeti@icloud.com>
2025-11-15 21:52:42 -0500

    Add Project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=50,
+        choices=[
+            ("DESIGN", "In design"),
+            ("READY", "Ready for deployment"),
+            ("ACTIVE", "Active"),
+        ],
+        default="DESIGN",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
